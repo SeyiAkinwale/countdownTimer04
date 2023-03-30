a1d7faa4c9e11b3909e3c13d96440d84995b5c17
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-03-30 21:17:06 +0000

    hello world script

---
+++ script.js
@@ -0,0 +1,11 @@
+//var start = document.getElementById('start');
+//var reset = dcoument.getEleemntById('reset');
+
+//var h = document.getElementById('hour');
+//var m = document.getElementById('minute');
+//var s = document.getElementById('sec');
+
+//start.addEventListener('click', function())
+{
+  alert("Hello World");
+}
